4ce5336f205adca4073ff3983cbf68e60d79e310
Simon Sablowski <contact@simsab.net>
2011-04-05 23:15:22 +0000

    - moved Importer logic to own library - ImporterController handles the configuration and request management only - added simple stylesheet for output - handling Errors

--- controllers/ImporterController.php
+++ controllers/ImporterController.php
@@ -1,131 +1,32 @@
 <?php
 
 class ImporterController extends Controller {
-	protected $CoachingId = 1;
-	protected $Coachings = array();
-	protected $Objects = array();
-	protected $ObjectTransitions = array();
-	protected $xmlStack = array();
-	protected $nodePointer;
+	protected $Importer;
 	
-	protected function truncateTables() {
-		return Database::query('TRUNCATE `object`') && Database::query('TRUNCATE `objecttransition`');
-	}
-	
-	protected function analyze($string) {
-		preg_match('/\$(\w+):(\w+)\((.*)\)(.*)/is', $string, $matches);
-		return array_map('trim', array_slice($matches, 1));
-	}
-	
-	//TODO
-	protected function handleGateway($node) {
-		return TRUE;
-	}
-	
-	//TODO
-	protected function handleQuestion($node) {
-		return array(
-			'type' => 'Options',
-			'key' => NULL,
-			'properties' => ''
-		);
-	}
-	
-	protected function pushOntoXmlStack($element) {
-		return $this->xmlStack[] = $element;
-	}
-	
-	protected function popOffXmlStack() {
-		return end($this->xmlStack);
-	}
-	
-	protected function getXmlBuffer() {
-		return $this->popOffXmlStack();
-	}
-	
-	protected function import($directory) {
-		$path = $this->getConfiguration('pathModeling') . $directory;
-		$path .= substr($path, -1) == '/' ? '' : '/';
-		$this->scanFile($path . 'Start.xpdl');
-	}
-	
-	protected function findNode($pattern) {
-		return $this->setNodePointer($this->getXmlBuffer()->xpath($pattern));
-	}
-	
-	protected function findStartNode() {
-		$pattern = '//Activity/Event/StartEvent/parent::*/parent::*';
-		return $this->setNodePointer(pos($this->getXmlBuffer()->xpath($pattern)));
-	}
-	
-	protected function findNextNodes() {
-		$id = $this->getNodePointer()->attributes()->Id;
-		$nodes = array();
-		$pattern = '//Transition[@From="%s"]';
-		foreach ($this->getXmlBuffer()->xpath(sprintf($pattern, $id)) as $transition) {
-			$pattern = '//Activity[@Id="%s"]/Implementation/Task/*/parent::*/parent::*/parent::*';
-			if ($array = $this->getXmlBuffer()->xpath(sprintf($pattern, $transition->attributes()->To))) {
-				$node = pos($array);
-				if ($this->registerNode($node)) $nodes[] = $node;
-			}
-		}
-		return $nodes;
-	}
-	
-	protected function registerNode($node) {
-		if (isset($this->Objects[$id = (string)$node->attributes()->Id])) {
-			return $this->Objects[$id];
+	public function setup($void = NULL) {
+		if (is_null($this->getConfiguration('pathModeling'))) {
+			throw new FatalError('Modeling path not set');
 		}
 		
-		$title = (string)$node->attributes()->Name;
-		
-		if (isset($node->Implementation->Task->TaskManual)) {
-			list($type, $key, $properties, $description) = $this->analyze((string)$node->Description);
-		} else if (isset($node->Implementation->Task->TaskScript)) {
-			$description = (string)$node->Description;
-			if ($title && !$description) {
-				$description = $title;
-				$title = '';
-			}
-			$type = 'Text';
-		} else if (isset($node->Implementation->Task->TaskReference)) {
-			list($type, $key, $properties) = $this->handleQuestion($node);
+		if (!is_null($this->getRequest()->getData('autoRun'))) {
+			
 		}
 		
-		$Object = new Object(array(
-			'CoachingId' => $this->getCoachingId(),
-			'title' => $title,
-			'description' => isset($description) ? $description : NULL,
-			'type' => $type,
-			'key' => isset($key) ? $key : NULL,
-			'properties' => isset($properties) ? sprintf('{%s}', $properties) : NULL
+		$Coaching = new Coaching(array(
+			'id' => 1,
+			'key' => 'psychotest',
+			'language' => 'de_DE',
+			'title' => 'Psychotest'
 		));
-		$Object->create();
-		$this->Objects[$id] = $Object;
+		$Coaching->create();
 		
-		return $Object;
-	}
-	
-	protected function traverseNodes() {
-		if (!$this->getNodePointer()) return FALSE;
-		$nodes = $this->findNextNodes();
-		foreach ($nodes as $node) {
-			$this->setNodePointer($node);
-			return $this->traverseNodes();
-		}
-		return $nodes;
-	}
-	
-	protected function scanFile($pathFile) {
-		$data = preg_replace('/(xmlns=")(.+)(")/', '$1$3', file_get_contents($pathFile));
-		$this->pushOntoXmlStack(new SimpleXMLElement($data));
-		$this->findStartNode();
-		return $this->traverseNodes();
+		$this->setImporter(new Importer($this->getConfiguration()));
+		$this->getImporter()->setCoachings(array($Coaching));
 	}
 	
 	public function index() {
-		$this->truncateTables();
-		$this->import('psychotest');
-		var_dump(count($this->getObjects()));
+		$this->setup();
+		$this->getImporter()->run();
+		var_dump(count($this->getImporter()->getObjects()));
 	}
 }
