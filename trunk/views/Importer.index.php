cb768f711f680d44b92f3387269e508236b500e1
simsab@simsab.de <simsab@simsab.de@99cf7004-7828-fcc1-1467-2d7c9ad312bd>
2011-04-09 03:01:49 +0000

    - corrected encoding in translations - displaying detailed information about imported Objects - adding helper view StdObject.php for displaying nested properties

--- trunk/views/Importer.index.php
+++ trunk/views/Importer.index.php
@@ -6,7 +6,7 @@
 			<table class="content">
 				<thead class="head">
 					<tr>
-						<th class="field">
+						<th class="field" colspan="5">
 							<? echo $this->localize('Successfully imported:'); ?>
 
 						</th>
@@ -14,29 +14,38 @@
 				</thead>
 				<tbody class="body">
 <? foreach ($Coachings as $n => $Coaching): ?>
-					<tr class="<? echo $n % 2 ? 'odd' : 'even'; ?>">
-						<td class="field">
-							<ol>
-								<li>
-									<p>
-										<? echo $Coaching->getKey(); ?>
+					<tr>
+						<td class="field data" colspan="5">
+							<? echo $Coaching->getKey(); ?> (<? echo $this->localize('%d objects', count($Coaching->getObjects())); ?>)
 
-									</p>
-									<p>
-										<? echo $this->localize('%d objects:', count($Coaching->getObjects())); ?>
-
-									</p>
-									<ol>
-<? foreach ($Coaching->getObjects() as $Object): ?>
-										<li>
-											<? echo $Object->getType(); ?><? if ($title = $Object->getTitle()): ?>: <? echo $title; ?><? endif; ?>
-
-										</li>
-<? endforeach; ?>
-									</ol>
-								</li>
-							</ol>
 						</td>
+					</tr>
+<? foreach ($Coaching->getObjects() as $m => $Object): ?>
+					<tr class="<? echo $m % 2 ? 'even' : 'odd'; ?>">
+						<td class="<? if ($m + 1 == count($Coaching->getObjects())): ?>last <? endif; ?>number">
+							<? echo $m + 1; ?>
+
+						</td>
+						<td class="field data">
+							<? echo $Object->getType(); ?>
+
+						</td>
+						<td class="field data">
+							<? echo $Object->getKey(); ?>
+
+						</td>
+						<td class="field">
+							<? if ($title = $Object->getTitle()): ?><? echo $title; ?><? else: ?><? echo $Object->getDescription(); ?><? endif; ?>
+
+						</td>
+						<td class="field">
+<? $this->displayView('components/StdObject.php', array(
+	'StdObject' => Json::decode($Object->getProperties()),
+	'indent' => 7
+)); ?>
+						</td>
+					</tr>
+<? endforeach; ?>
 					</tr>
 <? endforeach; ?>
 				</tbody>
