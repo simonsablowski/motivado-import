5b7b726369d55ad7a2bcf5c5d787c0303f824bc1
simsab@simsab.de <simsab@simsab.de@99cf7004-7828-fcc1-1467-2d7c9ad312bd>
2011-04-13 09:31:04 +0000

    - made Node::analyze method static to be able to call overwritten versions - fixed Transition finding for OptionsNode - output of object type in json-decoded StdObject

--- trunk/views/components/StdObject.php
+++ trunk/views/components/StdObject.php
@@ -18,10 +18,11 @@ if (!function_exists('dumpProperty')) {
 		} else if (is_string($value) || is_int($value)) {
 			return sprintf("%s<tr>\n%s<td>\n%s%s\n%s</td>\n%s<td>\n%s%s\n%s</td>\n%s</tr>\n", tabs($indent + 1), tabs($indent + 2), tabs($indent + 3), $property, tabs($indent + 2), tabs($indent + 2), tabs($indent + 3), $value, tabs($indent + 2), tabs($indent + 1));
 		} else if (is_object($value)) {
-			$dump = '';
+			$dump = sprintf("%s<tr>\n%s<td>\n%s%s\n%s</td>\n%s<td>\n%s<table>\n", tabs($indent + 1), tabs($indent + 2), tabs($indent + 3), $property, tabs($indent + 2), tabs($indent + 2), tabs($indent + 3));
 			foreach (get_object_vars($value) as $p => $v) {
 				$dump .= dumpProperty($p, $v, $indent + 1);
 			}
+			$dump .= sprintf("%s</table>\n%s</td>\n%s</tr>\n", tabs($indent + 3), tabs($indent + 2), tabs($indent + 1));
 			return $dump;
 		}
 		return NULL;
