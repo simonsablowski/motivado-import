756cc011ad0378972e2a744285e4974219c17a30
Simon Sablowski <contact@simsab.net>
2011-07-28 14:19:38 +0000

    no longer displaying unreadable (too long) Error trace

--- views/cli/Error.show.php
+++ views/cli/Error.show.php
@@ -2,12 +2,12 @@
 echo $this->localize('Unfortunately,') . ' ' . $this->localize('we encountered an error:') . "\n";
 $fields = array('Type', 'Message');
 if ($this->getApplication()->getConfiguration('debugMode')) {
-	$fields = array_merge($fields, array('Details', 'Trace'));
+	$fields = array_merge($fields, array('Details'));
 }
 foreach ($fields as $n => $field) {
 	echo $this->localize($field) . ': ';
 	$getter = 'get' . $field;
-	if ($field != 'Details' && $field != 'Trace') {
+	if ($field != 'Details') {
 		echo $this->localize($Error->$getter());
 	} else {
 		var_dump($Error->$getter());
